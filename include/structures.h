32e6fd9e85de3c49e59604c07cef85b17f685076
Cris Van Schijndel <cvan-sch@f1r6s12.codam.nl>
2024-06-04 16:05:38 +0200

    add 'all' data structure which contains all structs (can modify later)

--- include/structures.h
+++ include/structures.h
@@ -1,31 +1,40 @@
 #ifndef STRUCTURES_H
 # define STRUCTURES_H
 
-typedef struct	player_data
+typedef struct	s_player
 {
 	int			position[2];
 	int			direction[2];
 	int			plane[2];
-};
+}				t_player;
 
-typedef struct	ray_data
+typedef struct	s_ray
 {
 	int			side_dist[2];
 	int			delta_dist[2];
 	int			hit;
 	int			side;
-};
+}				t_ray;
 
-typedef struct	map_data
+typedef struct	s_map
 {
 	int			**map;
 	int			index[2];
-};
+}				t_map;
 
-typedef struct	fps
+typedef struct	s_fps
 {
 	double		time;
 	double		old_time;
-};
+}				t_fps;
+
+typedef struct	s_data
+{
+	t_player	player;
+	t_ray		ray;
+	t_map		map;
+	t_fps		fps;
+}				t_data;
+
 
 #endif
