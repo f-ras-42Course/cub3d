9ee08cfa69e664aa7011c5a6dd38da4899aab2c2
Cris Van Schijndel <cvan-sch@f1r6s12.codam.nl>
2024-06-04 15:33:17 +0200

    added header file for parsing and the main parsing c file

---
+++ include/parse.h
@@ -0,0 +1,24 @@
+
+#ifndef PARSE_H
+# define PARSE_H
+
+/*
+one struct with all data or multiple structs
+- player data
+	-position (x, y)
+	-direction (x, y)
+	-plane (x, y)
+
+- map data
+	-2d array for map (int *)
+	-mapcoordinates (x, y)
+
+- ray data
+	-sidedistance (x, y)
+	-deltadistance (x, y)
+	-hit (binary)
+	-side (binary)
+*/
+
+#endif
+
---
+++ src/parse/parse_map.c
@@ -0,0 +1,6 @@
+#include "parse.h"
+
+int	parse_map(void)
+{
+	
+}
