c827e5931c44f0b20353e918ca2e182ab1ff8579
Cris Van Schijndel <cvan-sch@f1r6s12.codam.nl>
2024-06-05 14:03:42 +0200

    add a global variable in error_handling for error_messages (very responsible)

--- src/utils/error_handling.c
+++ src/utils/error_handling.c
@@ -1,19 +1,19 @@
 
 #include "cub3d.h"
 
+static const char	*g_error_msg[] = {
+	"Succes",
+	// PARSE
+	"make sure the map ends with '.cub'",
+	"no map supplied",
+	// GFX
+};
 
 /*a function that exits with a custom exit_code and message
 will write to standard error and adds a new_line.*/
 void	error(int error_code, t_data *all)
 {
-	static const char	*error_msg[] = {
-		"Succes",
-		// PARSE
-		"make sure the map ends with '.cub'",
-		"no map supplied",
-		// GFX
-	};
 	all->error_code = error_code;
 	write(2, "Error: ", 7);
-	ft_putendl_fd((char *)error_msg[error_code], STDERR_FILENO);
+	ft_putendl_fd((char *)g_error_msg[error_code], STDERR_FILENO);
 }
