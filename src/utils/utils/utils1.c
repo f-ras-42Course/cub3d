0f4acc17f6d888060b5c1f80598772c403513a50
Cris Van Schijndel <cvan-sch@f1r6s12.codam.nl>
2024-06-05 12:14:20 +0200

    add strnstr to utils1.c

--- src/utils/utils/utils1.c
+++ src/utils/utils/utils1.c
@@ -1,5 +1,6 @@
 
 #include "cub3d.h"
+#include <stddef.h>
 
 size_t	ft_strlen(const char *s)
 {
@@ -15,4 +16,26 @@ void	ft_putendl_fd(char *s, int fd)
 {
 	write(fd, s, ft_strlen(s));
 	write(fd, "\n", 1);
+}
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t		i;
+	size_t		j;
+
+	i = 0;
+	if (needle[i] == '\0')
+		return ((char *)haystack);
+	while (i < len && haystack[i])
+	{
+		j = 0;
+		while ((haystack[i + j] == needle[j]) && (i + j < len))
+		{
+			if (!needle[j + 1])
+				return ((char *)&haystack[i]);
+			j++;
+		}
+		i++;
+	}
+	return (0);
 }
