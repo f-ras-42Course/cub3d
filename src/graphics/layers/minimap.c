03154886edc9efbd2842b8558bed289ca1186cb8
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2024-06-09 14:55:00 +0200

    removed commented out function and prepped for global constant

--- src/graphics/layers/minimap.c
+++ src/graphics/layers/minimap.c
@@ -1,55 +1,36 @@
 
 #include "graphics.h"
 
-// bool	init_minimap(t_gfx_data *graphics)
-// {
-// 	const int	measures[4] = {
-// 	[RECT_WIDTH] = (SCREEN_WIDTH / 8),
-// 	[RECT_HEIGHT] = (SCREEN_HEIGHT / 6),
-// 	[DRAW_POS_X] = 0,
-// 	[DRAW_POS_Y] = 0
-// 	};
-
-// 	graphics->minimap = rect_image_draw(graphics->mlx,
-// 							measures, 0xFF0000ff);
-// 	if (!graphics->minimap)
-// 		return (false);
-// 	return (true);
-// }
-
 /* Temporary varriant on the final minimap*/
 bool	init_minimap(t_gfx_data *graphics)
 {
-	const int	minimap_width = (SCREEN_WIDTH / 8);
-	const int	minimap_height = (SCREEN_HEIGHT / 6);
+
 
 	graphics->minimap = rect_image(graphics->mlx, \
-						minimap_width, minimap_height);
+						g_minimap_width, g_minimap_height);
 	if (!graphics->minimap)
 		return (false);
 	draw_minimap_frame(graphics->minimap);
+	// draw_minimap_player(graphics->minimap);
 	// draw_minimap_walls(graphics);
 	return (true);
 }
 
 void	draw_minimap_frame(mlx_image_t *minimap)
 {
-	const int	minimap_width = (SCREEN_WIDTH / 8);
-	const int	minimap_height = (SCREEN_HEIGHT / 6);
-	const int	minimap_frame_thickness = 5;
 	int	x;
 	int	y;
 
 	x = 0;
 	y = 0;
-	while (y < minimap_height)
+	while (y < g_minimap_height)
 	{
-		while (x < minimap_width)
+		while (x < g_minimap_width)
 		{
-			if ((y < minimap_frame_thickness \
-				|| x < minimap_frame_thickness \
-				|| x > (minimap_width - minimap_frame_thickness) \
-				|| y > (minimap_height - minimap_frame_thickness)))
+			if ((y < g_minimap_frame_thickness \
+				|| x < g_minimap_frame_thickness \
+				|| x > (g_minimap_width - g_minimap_frame_thickness) \
+				|| y > (g_minimap_height - g_minimap_frame_thickness)))
 				mlx_put_pixel(minimap, x, y, 0xff0000ff);
 			x++;
 		}
@@ -57,6 +38,12 @@ void	draw_minimap_frame(mlx_image_t *minimap)
 		y++;
 	}
 }
+
+// void	draw_minimap_player(mlx_image_t *minimap)
+// {
+// 	const int	minimap_width = (SCREEN_WIDTH / 8);
+// 	const int	minimap_height = (SCREEN_HEIGHT / 6);
+// }
 
 // void	draw_minimap_walls(t_gfx_data *graphics)
 // {
