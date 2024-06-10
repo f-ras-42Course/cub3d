ba2195108aaf57d5e596fc1106b2b23ce04f5b36
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2024-06-10 19:42:14 +0200

    fixed find player position

--- src/data/collect.c
+++ src/data/collect.c
@@ -14,11 +14,11 @@ void	set_start_position_player(t_all *data)
 
 	x = 0;
 	y = -1;
-	while (y < MAP_HEIGHT && !player_found(g_temp_test_map[x][y]))
+	while (y < MAP_HEIGHT && !player_found(g_temp_test_map[y][x]))
 	{
 		x = 0;
 		y++;
-		while (x < MAP_WIDTH && !player_found(g_temp_test_map[x][y]))
+		while (x < MAP_WIDTH && !player_found(g_temp_test_map[y][x]))
 		{
 			x++;
 		}
@@ -30,6 +30,3 @@ bool	player_found(char c)
 {
 	return (c == 'N' || c == 'E' || c == 'S' || c == 'W');
 }
-
-	// #include <stdio.h>
-	// printf("x: %d, y: %d\n", x, y);
