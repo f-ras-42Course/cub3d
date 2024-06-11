5f6616b39fdd90d99b1162da099bc0fbd3d7beaa
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2024-06-11 16:09:22 +0200

    removed unused get_rgba function

--- src/graphics/layers/minimap.c
+++ src/graphics/layers/minimap.c
@@ -1,13 +1,6 @@
 
 #include "graphics.h"
 
-/* Temporary varriant on the final minimap*/
-
-int get_rgba(int r, int g, int b, int a)
-{
-    return (r << 24 | g << 16 | b << 8 | a);
-}
-
 bool	init_minimap(t_gfx_data *graphics)
 {
 
