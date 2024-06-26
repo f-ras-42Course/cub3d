218eaa8abaebc2d74ab722020f00a5487a011c8f
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2024-06-26 13:24:11 +0200

    fix draw circle and small improvement

--- src/graphics/utils/circle.c
+++ src/graphics/utils/circle.c
@@ -11,9 +11,9 @@ void	draw_circle(mlx_image_t *image, \
 	float	rotation_step;
 
 	i = 0;
-	rotation = 0;
+	rotation = (2 * M_PI) / (2 * measures[RADIUS] * M_PI);
 	rotation_step = (2 * M_PI) / (2 * measures[RADIUS] * M_PI);
-	while (i < measures[RADIUS] * 100)
+	while (i < measures[RADIUS] * 50)
 	{
 		x = measures[DRAW_POS_CENTER_X] + measures[RADIUS] * cos(rotation);
 		y = measures[DRAW_POS_CENTER_Y] + measures[RADIUS] * sin(rotation);
