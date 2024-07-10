cc56d0199d1e2575e6b38333ea75cee8d6bbdb26
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2024-07-10 12:08:56 +0200

    first step towards multiple lines for bigmap line drawing

--- src/graphics/layers/bigmap/bigmap_line.c
+++ src/graphics/layers/bigmap/bigmap_line.c
@@ -1,16 +1,24 @@
 
 #include "graphics.h"
 
-void	draw_line(t_bigmap *bigmap, int color)
+void	bigmap_draw_single_line(t_bigmap *bigmap, int color)
+{
+	t_ray	ray;
+	double	bigmap_position[2];
+
+	bigmap_position[X] = bigmap->player->position[X] * bigmap->unit_size;
+	bigmap_position[Y] = bigmap->player->position[Y] * bigmap->unit_size;
+	init_line_variables(bigmap, &ray);
+	draw_single_line(bigmap, &ray, bigmap_position, color);
+}
+
+void	draw_single_line(t_bigmap *bigmap, t_ray *ray, double bigmap_position[2], int color)
 {
 	int	i;
 	int	line_size;
-	int	bigmap_position[2];
 
 	i = 0;
-	line_size = calculate_line_size(bigmap);
-	bigmap_position[X] = bigmap->player->position[X] * bigmap->unit_size;
-	bigmap_position[Y] = bigmap->player->position[Y] * bigmap->unit_size;
+	line_size = calculate_line_size(bigmap, *ray);
 	while (i <= line_size)
 	{
 		mlx_put_pixel(bigmap->image, \
@@ -18,6 +26,29 @@ void	draw_line(t_bigmap *bigmap, int color)
 		bigmap_position[Y] + i * sin(bigmap->player->direction[Y]), \
 		color);
 		i++;
+	}
+}
+
+int	calculate_line_size(t_bigmap *bigmap, t_ray ray)
+{
+	while (1)
+	{
+		if (ray.shortest[X] < ray.shortest[Y])
+		{
+			ray.check_pos[X] += copysign(1, cos(bigmap->player->direction[X]));
+			if (wall_found(ray.check_pos[X], ray.check_pos[Y]))
+				return (ray.shortest[X] * bigmap->unit_size);
+			ray.increment[X]++;
+			ray.shortest[X] = ray.increment[X] * ray.delta[X];
+		}
+		else
+		{
+			ray.check_pos[Y] += copysign(1, sin(bigmap->player->direction[Y]));
+			if (wall_found(ray.check_pos[X], ray.check_pos[Y]))
+				return (ray.shortest[Y] * bigmap->unit_size);
+			ray.increment[Y]++;
+			ray.shortest[Y] = ray.increment[Y] * ray.delta[Y];
+		}
 	}
 }
 
@@ -41,31 +72,4 @@ void	init_line_variables(t_bigmap *bigmap, t_ray *ray)
 	ray->shortest[Y] = ray->increment[Y] * ray->delta[Y];
 	ray->check_pos[X] = bigmap->player->position[X];
 	ray->check_pos[Y] = bigmap->player->position[Y];
-
-}
-
-int	calculate_line_size(t_bigmap *bigmap)
-{
-	t_ray	ray;
-
-	init_line_variables(bigmap, &ray);
-	while (1)
-	{
-		if (ray.shortest[X] < ray.shortest[Y])
-		{
-			ray.check_pos[X] += copysign(1, cos(bigmap->player->direction[X]));
-			if (wall_found(ray.check_pos[X], ray.check_pos[Y]))
-				return (ray.shortest[X] * bigmap->unit_size);
-			ray.increment[X]++;
-			ray.shortest[X] = ray.increment[X] * ray.delta[X];
-		}
-		else
-		{
-			ray.check_pos[Y] += copysign(1, sin(bigmap->player->direction[Y]));
-			if (wall_found(ray.check_pos[X], ray.check_pos[Y]))
-				return (ray.shortest[Y] * bigmap->unit_size);
-			ray.increment[Y]++;
-			ray.shortest[Y] = ray.increment[Y] * ray.delta[Y];
-		}
-	}
 }
