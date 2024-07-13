1e3eae43f41178d465f52a74a7290db484ed01a2
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2024-07-13 20:04:38 +0200

    refactor: change line lengths

--- include/graphics.h
+++ include/graphics.h
@@ -5,7 +5,7 @@
 #  define M_PI 	3.14159265358979323846
 # endif
 
-# define RD		0.01745329251 // (M_PI / 180)
+# define RD		0.01745329251 // = M_PI / 180
 
 # include "MLX42/MLX42.h"
 # include "definitions.h"
@@ -30,19 +30,25 @@ bool		init_bigmap(t_gfx_data *graphics);
 void		init_bigmap_values(t_bigmap *bigmap);
 
 // - Raycasting
-void		raycasting(t_mainlayer *mainlayer, uint32_t ceiling_color, uint32_t floor_color, uint32_t wall_color[4]);
+void		raycasting(t_mainlayer *mainlayer, uint32_t ceiling_color, \
+						uint32_t floor_color, uint32_t wall_color[4]);
 void		init_ray_variables(const t_player *player, t_ray *ray);
 double		ray_distance(t_ray *ray);
 void		set_wall_side(t_ray *ray, int side_check);
 void		place_full_ceiling_colored(mlx_image_t *image, uint32_t color);
 void		place_full_floor_colored(mlx_image_t *image, uint32_t color);
-void		place_wall_colored(mlx_image_t *image, int wall_height, int position, uint32_t color);
+void		place_wall_colored(mlx_image_t *image, int wall_height, \
+								int position, uint32_t color);
 
 // -- Single Raycasting (for testing)
-void		single_raycasting(t_mainlayer *mainlayer, uint32_t ceiling_color, uint32_t floor_color);
-void		single_place_ceiling(mlx_image_t *image, int wall_height, int position, uint32_t color);
-void		single_place_wall(mlx_image_t *image, int wall_height, int position, uint32_t color);
-void		single_place_floor(mlx_image_t *image, int wall_height, int position, uint32_t color);
+void		single_raycasting(t_mainlayer *mainlayer, uint32_t ceiling_color, \
+								uint32_t floor_color);
+void		single_place_ceiling(mlx_image_t *image, int wall_height, \
+									int position, uint32_t color);
+void		single_place_wall(mlx_image_t *image, int wall_height, \
+								int position, uint32_t color);
+void		single_place_floor(mlx_image_t *image, int wall_height, \
+								int position, uint32_t color);
 
 // - Minimap
 void		minimap(t_minimap *minimap);
@@ -57,13 +63,14 @@ bool		is_minimap_unit_out_of_map_scope(const t_minimap *minimap);
 void		reset_minimap_end_of_map_locator(t_minimap *minimap);
 
 // - Minimap framworks (for testing)
-void		draw_minimap_frame(const t_minimap *minimap, t_minimap_options option);
+void		draw_minimap_frame(const t_minimap *minimap, \
+								t_minimap_options option);
 
 // - Bigmap
 void		bigmap(const t_bigmap *bigmap);
 void		draw_walls_on_bigmap(const t_bigmap *bigmap);
 void		fill_bigmap_unit(const t_bigmap *bigmap, int bigmap_pos_x, \
-			int bigmap_pos_y, int color);
+								int bigmap_pos_y, int color);
 bool		is_wall_for_bigmap(const t_bigmap *bigmap);
 void		draw_player_on_bigmap(const t_bigmap *bigmap, int color);
 
@@ -71,7 +78,7 @@ void		draw_player_on_bigmap(const t_bigmap *bigmap, int color);
 
 void		bigmap_draw_ray_lines(const t_bigmap *bigmap, int color);
 void		draw_single_ray_line(const t_bigmap *bigmap, t_ray *ray, \
-			double bigmap_position[2], int color);
+									double bigmap_position[2], int color);
 void		init_ray_line_variables(const t_player *player, t_ray *ray);
 int			calculate_line_size(const t_bigmap *bigmap, t_ray *ray);
 bool		wall_found(int x, int y);
@@ -83,19 +90,19 @@ void		draw_bigmap_raster(const t_bigmap *bigmap);
 // - Rectangle
 
 mlx_image_t	*rect_image_draw(mlx_t *mlx, \
-			const int measures[4], unsigned int color);
+								const int measures[4], unsigned int color);
 void		draw_rect(mlx_image_t *image, \
 						const int measures[4], unsigned int color);
 
 // - Circle
 
 void		draw_circle(mlx_image_t *image, \
-			const int measures[3], unsigned int color);
+							const int measures[3], unsigned int color);
 void		draw_filled_circle(mlx_image_t *image, \
-			const int measures[3], unsigned int color);
+								const int measures[3], unsigned int color);
 
 // - Rendering
 
-void	update_image(t_gfx_data *graphics);
+void		update_image(t_gfx_data *graphics);
 
 #endif
