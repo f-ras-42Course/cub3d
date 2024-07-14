13433a29367e881d2a0caa6194728dfa0d812957
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2024-07-14 20:44:07 +0200

    out-commented parsing - until fixed

--- src/main.c
+++ src/main.c
@@ -1,11 +1,12 @@
+
 #include "cub3d.h"
 
 int	main(int argc, char **argv)
 {
 	t_all	data;
 
-	if (!parse_map(&data, argc, argv[1]))
-		return (data.error_code);
+	// if (!parse_map(&data, argc, argv[1]))
+	// 	return (data.error_code);
 	set_data(&data);
 	if (!init_graphics(&data))
 		return (data.error_code);
