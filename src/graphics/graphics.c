c838217d02d3a10196ad5693938980f4345ad5d3
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2024-07-24 20:46:10 +0200

    fixed undefined due to uninitialized pointer

--- src/graphics/graphics.c
+++ src/graphics/graphics.c
@@ -6,12 +6,12 @@ bool	init_graphics(t_all *data)
 	data->graphics.mlx = mlx_init(SCREEN_WIDTH, SCREEN_HEIGHT, "Cub3d", true);
 	if (!data->graphics.mlx)
 		return (error(MLX_GFX_CRASH, data), false);
-	if (!init_image(&data->graphics))
-		return (error(MLX_GFX_CRASH, data), false);
-	data->graphics.data = data;
 	data->graphics.minimap.player = &(data->player);
 	data->graphics.bigmap.player = &(data->player);
 	data->graphics.mainlayer.player = &(data->player);
+	if (!init_image(&data->graphics))
+		return (error(MLX_GFX_CRASH, data), false);
+	data->graphics.data = data;
 	return (true);
 }
 
